3092e85049e129601db5a3cba46bcc2cf8b9c01a
Viktor Novak <valakar@gmail.com>
2017-03-29 23:13:11 +0300

    [NV-3] Init footer and logo

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-footer',
-    templateUrl: 'footer.template.html'
+    templateUrl: 'footer.template.html',
+    styleUrls: ['footer.styles.sass']
 })
 export class FooterComponent {
     constructor() { }
